74648414e474a74742356bd72d60f4ec04ebe2e8
Gokulnaath Govindaraj <gokulhallows003@gmail.com>
2026-02-04 20:07:05 -0500

    I included a function to figure out whether its a header or not. 1.if text is captal and less than 80 characters 2. If it starts with some words 3. If the 1,2, sec like that comes up and if we split the text and it has less than 10 words also not end up with . also the first letter is caps and if there are more than 2 caps in the text then YES.

--- pipeline/pipeline_flow.py
+++ pipeline/pipeline_flow.py
@@ -5,7 +5,7 @@ from services.extractor import parse_pdf_blocks
 from services.chunking import returning_allchunks
 
 
-parse=parse_pdf_blocks(r"storage\docs\f081b0e2-5fd6-4100-89f9-b7f5f49874b0\occidental_ars.pdf")
+parse=parse_pdf_blocks(r"storage\docs\f081b0e2-5fd6-4100-89f9-b7f5f49874b0\Apple 10-k.pdf")
 result=parse.get("sections", [])
 
 for s in result:
